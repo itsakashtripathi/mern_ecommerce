670c3980ad31f29f7e493d8766bef16170c0f6c2
itsakashtripathi <98697978+itsakashtripathi@users.noreply.github.com>
2022-04-24 22:47:00 +0530

    Avatar Component

---
+++ frontend/src/components/Avatar/Avatar.jsx
@@ -0,0 +1,12 @@
+import './avatar.css';
+import guestUserImg from '../../assets/images/guest_user.png';
+
+const Avatar = ({avatar}) => {
+    return (
+        <div className='avatar_container'>
+            <img className='avatar_img' src={avatar.img ? avatar.img : guestUserImg} alt='' draggable='false' />
+        </div>
+    );
+}
+
+export default Avatar;
