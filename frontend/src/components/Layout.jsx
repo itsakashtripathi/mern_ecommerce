43ecc26126750857ec4fa4bf3f6daefd1c5da734
itsakashtripathi <98697978+itsakashtripathi@users.noreply.github.com>
2022-04-25 15:37:26 +0530

    Main Component

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import Navbar from './Navbar/Navbar';
 import Footer from './Footer/Footer';
+import Main from './Main/Main';
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Navbar />
+      <Main />
       <Footer />
       {children}
     </div>
