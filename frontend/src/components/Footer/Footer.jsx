ae3c9c57bf14be1bce48c8cc9573c073ffad63c4
itsakashtripathi <98697978+itsakashtripathi@users.noreply.github.com>
2022-04-26 23:31:33 +0530

    Minor Fix

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,15 +1,23 @@
-import './footer.css';
-import Logo from '../../assets/images/amazona_logo_32.png';
+import "./footer.css";
+import Logo from "../../assets/images/amazona_logo_32.png";
 
 export default function Footer() {
   return (
-    <div className='footer'>
-        <img src={Logo} className="d-inline-block align-text-top pointer" alt='' draggable='false' />
-        <p>
-            © All rights reserved
-            <br/>
-            made by <a href='#' className='link'>Akash Tripathi</a>
-        </p>
+    <div className="footer">
+      <img
+        src={Logo}
+        className="d-inline-block align-text-top pointer"
+        alt=""
+        draggable="false"
+      />
+      <p>
+        © All rights reserved
+        <br />
+        Made by{" "}
+        <a href="#" className="link">
+          Akash Tripathi
+        </a>
+      </p>
     </div>
-  )
+  );
 }
