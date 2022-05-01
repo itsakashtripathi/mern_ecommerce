e6ce7a587fdd3b87ae49cca070ef55616177fc78
itsakashtripathi <98697978+itsakashtripathi@users.noreply.github.com>
2022-05-01 15:29:07 +0530

    Added Gap Component

---
+++ frontend/src/components/Gap/Gap.jsx
@@ -0,0 +1,7 @@
+export default function Gap(props) {
+  return (
+    <div className="container">
+      <div style={{ marginTop: props.gap }}></div>
+    </div>
+  );
+}
--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
-import Products from '../Products/Products';
-import './main.css';
+import Gap from "../Gap/Gap";
+import Products from "../Products/Products";
+import "./main.css";
 
 export default function Main() {
   return (
-    <div className='container'>
-        <Products />
+    <div className="container">
+      <Gap gap={"100px"} />
+      <Products />
     </div>
-  )
+  );
 }
